ad6a05664b471fd2c04da5302fa5962a1f015402
Kevin Bazilio <disckevinbf@gmail.com>
2023-06-20 15:06:45 -0300

    :tada: Glasses Modify

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function toggleMode(){
   // substituir a imagem
   if (html.classList.contains("light")){
     // se tiver light mode, adicionar a imgagem light
-    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('src', './assets/Avatar-Light.png')
     img.setAttribute("alt", "Foto de Kevin usando óculos")
   }
   else{
     // se estiver sem o light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-Dark.png")
     img.setAttribute("alt", "Foto de Kevin sem óculos")
   }
     
